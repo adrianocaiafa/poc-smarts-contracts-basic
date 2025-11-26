4da64633c4a23340fc9e6aa8c4c951e0a46a1fb3
adrianocaiafa <acaiafa@hotmail.com>
2025-11-26 16:45:39 -0300

    add more WIZARD STATS

--- contracts/WizardLevelRanking.sol
+++ contracts/WizardLevelRanking.sol
@@ -16,4 +16,10 @@ contract WizardLevelRanking {
     mapping(address => uint256) public level;       // current level
     mapping(address => uint256) public mana;        // current mana
     mapping(address => uint256) public spellsCast;  // total spells cast
+
+    address[] internal wizards; // list of all wizards that ever interacted
+
+    uint256 public constant XP_PER_LEVEL   = 100;  // 100 XP per level
+    uint256 public constant MANA_PER_LEVEL = 10;   // bonus mana per level up
+    uint256 public constant BASE_SPELL_COST = 5;   // default mana cost for spells
 }
