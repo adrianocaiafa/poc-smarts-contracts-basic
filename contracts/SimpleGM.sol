312856b86cba98e751b777136ca21ae77bb48136
adrianocaiafa <acaiafa@hotmail.com>
2025-12-03 09:26:42 -0300

    simpleGM GM data

--- contracts/SimpleGM.sol
+++ contracts/SimpleGM.sol
@@ -9,4 +9,28 @@ contract SimpleGM {
     uint256 public totalUniqueUsers;
     mapping(address => bool) public hasInteracted;
     mapping(address => uint256) public interactionsCount;
+
+    // ====== Dados específicos de GM ======
+
+    // Quantos GMs esse address já deu no total
+    mapping(address => uint256) public gmCount;
+
+    // "Dia" (em número inteiro) do último GM dado por esse address
+    mapping(address => uint256) public lastGMDay;
+
+    // Streak atual de dias seguidos dando GM
+    mapping(address => uint256) public currentStreak;
+
+    // Melhor streak já atingida
+    mapping(address => uint256) public bestStreak;
+
+    error AlreadyGmToday();
+
+    event GoodMorning(
+        address indexed user,
+        uint256 day,
+        uint256 gmCount,
+        uint256 currentStreak,
+        uint256 bestStreak
+    );    
 }
