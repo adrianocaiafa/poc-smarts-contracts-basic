d2f378bdeb0a1b777771e6d3c60efd12ff1cd19a
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 14:44:26 -0300

    feat: SimpleKudos add giveKudos write

--- contracts/SimpleKudos.sol
+++ contracts/SimpleKudos.sol
@@ -35,5 +35,30 @@ contract SimpleKudos {
         uint256 senderInteractions,
         uint256 totalUniqueUsers,
         uint256 totalKudos
-    );    
+    );
+
+    // -------------------------------------------------------------------------
+    // WRITE FUNCTIONS
+    // -------------------------------------------------------------------------
+
+    /// @notice Send 1 kudos point to a given address
+    function giveKudos(address _to) external {
+        require(_to != address(0), "Invalid address");
+        require(_to != msg.sender, "Cannot send to self");
+
+        _registerInteraction();
+
+        kudosSent[msg.sender] += 1;
+        kudosReceived[_to] += 1;
+        totalKudos += 1;
+
+        emit KudosSent(
+            msg.sender,
+            _to,
+            1,
+            interactionsCount[msg.sender],
+            totalUniqueUsers,
+            totalKudos
+        );
+    }     
 }
