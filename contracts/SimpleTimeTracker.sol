26c547ba6240ccbb715ca887e964fb49547e235c
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 19:55:07 -0300

    feat: SimpleTimeTracker add generic metrics and stats

--- contracts/SimpleTimeTracker.sol
+++ contracts/SimpleTimeTracker.sol
@@ -4,4 +4,21 @@ pragma solidity ^0.8.30;
 /// @title SimpleTimeTracker – on-chain check-ins with timestamps per user
 /// @author acaiafa.base.eth
 contract SimpleTimeTracker {
+    // -------------------------------------------------------------------------
+    // GENERIC INTERACTION METRICS
+    // -------------------------------------------------------------------------
+
+    uint256 public totalUniqueUsers;
+    mapping(address => bool) public hasInteracted;
+    mapping(address => uint256) public interactionsCount;
+
+    // -------------------------------------------------------------------------
+    // CHECK-IN STATE
+    // -------------------------------------------------------------------------
+
+    mapping(address => uint256) public checkInCount;
+    mapping(address => uint256) public lastCheckInAt;
+
+    uint256 public totalCheckIns;
+
 }
