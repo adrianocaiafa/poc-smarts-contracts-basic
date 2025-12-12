c62387c694fd7d2473e3ba160d6d901e12d12270
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 14:42:35 -0300

    feat: create SimpleKudos

---
+++ contracts/SimpleKudos.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @title SimpleKudos – send simple on-chain kudos between addresses
+/// @author acaiafa.base.eth
+contract SimpleKudos {
+}
