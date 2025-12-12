d21807c2634b2983a6833a6828504219eb945262
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 11:13:10 -0300

    feat: SimpleVoting structure basic

--- contracts/SimpleVoting.sol
+++ contracts/SimpleVoting.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.20;
 /// @title SimpleVoting
 /// @notice Yes/No voting on a single proposal
 contract SimpleVoting {
+    string public proposal;
+    uint256 public yesVotes;
+    uint256 public noVotes;
 
+    mapping(address => bool) public hasVoted;
 }
