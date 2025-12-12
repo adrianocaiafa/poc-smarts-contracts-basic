10f45b716595e20000dceb104e84ce727a1e2a9e
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 18:52:48 -0300

    feat: SimpleBookmarks add generic metrics

--- contracts/SimpleBookmarks.sol
+++ contracts/SimpleBookmarks.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.30;
 /// @title SimpleBookmarks – on-chain single bookmark per user (URL or reference)
 /// @author acaiafa.base.eth
 contract SimpleBookmarks {
+    // -------------------------------------------------------------------------
+    // GENERIC INTERACTION METRICS
+    // -------------------------------------------------------------------------
+
+    uint256 public totalUniqueUsers;
+    mapping(address => bool) public hasInteracted;
+    mapping(address => uint256) public interactionsCount;    
 }
