be139062925c49d56654eae1a8ef630687a6700d
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 11:40:57 -0300

    feat: SimpleDiary add struct Entry

--- contracts/SimpleDiary.sol
+++ contracts/SimpleDiary.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.8.20;
 
 /// @title SimpleDiary
 /// @notice Each user can store a single diary entry on-chain
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title SimpleDiary
+/// @notice Each user can store a single diary entry on-chain
 contract SimpleDiary {
+    struct Entry {
+        string text;
+        uint256 updatedAt;
+    }
 }
