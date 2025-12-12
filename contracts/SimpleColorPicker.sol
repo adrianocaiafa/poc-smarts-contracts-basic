df7d3356aa9cc12ede84e25c60e8a0cf9048c347
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 15:00:44 -0300

    feat: create SimpleColorPicker

---
+++ contracts/SimpleColorPicker.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @title SimpleColorPicker – on-chain favorite color per user (RGB uint24)
+/// @author acaiafa.base.eth
+contract SimpleColorPicker {
+}
