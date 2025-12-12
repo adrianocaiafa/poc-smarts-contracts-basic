78ce508db531aa78c320f50806e92cf528742651
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 19:54:34 -0300

    feat: create SimpleTimeTracker

---
+++ contracts/SimpleTimeTracker.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @title SimpleTimeTracker – on-chain check-ins with timestamps per user
+/// @author acaiafa.base.eth
+contract SimpleTimeTracker {
+}
