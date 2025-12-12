ef2426451f6479b4975a6a78f07411b16f8d2974
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 11:31:21 -0300

    feat: SimplePing add structure basic

--- contracts/SimplePing.sol
+++ contracts/SimplePing.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.20;
 /// @title SimplePing
 /// @notice Minimal contract to record pings per user
 contract SimplePing {
+mapping(address => uint256) public pingCount;
+    mapping(address => uint256) public lastPingAt;
+
+    uint256 public totalPings;    
 }
