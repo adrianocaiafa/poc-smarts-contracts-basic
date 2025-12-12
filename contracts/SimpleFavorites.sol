49ae8bbb283d125070f9ada5c0c6ca2027773350
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 20:01:40 -0300

    feat: create SimpleFavorites

---
+++ contracts/SimpleFavorites.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+/// @title SimpleFavorites – on-chain favorite number per user
+/// @author acaiafa.base.eth
+contract SimpleFavorites {
+}
