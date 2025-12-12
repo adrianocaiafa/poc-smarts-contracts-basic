90533e5cbec0ad5838de527cb13b425362c18b66
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 14:43:02 -0300

    feat: SimpleKudos add generic metrics

--- contracts/SimpleKudos.sol
+++ contracts/SimpleKudos.sol
@@ -4,4 +4,12 @@ pragma solidity ^0.8.30;
 /// @title SimpleKudos – send simple on-chain kudos between addresses
 /// @author acaiafa.base.eth
 contract SimpleKudos {
+    // -------------------------------------------------------------------------
+    // GENERIC INTERACTION METRICS
+    // -------------------------------------------------------------------------
+
+    uint256 public totalUniqueUsers;
+    mapping(address => bool) public hasInteracted;
+    mapping(address => uint256) public interactionsCount;
+
 }
