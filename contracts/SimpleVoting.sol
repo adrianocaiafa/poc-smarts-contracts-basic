ede027451699dc3b3c1f32ad35eaca341a8c00e6
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 11:14:04 -0300

    feat: SimpleVoting add vote function

--- contracts/SimpleVoting.sol
+++ contracts/SimpleVoting.sol
@@ -11,4 +11,20 @@ contract SimpleVoting {
     mapping(address => bool) public hasVoted;
 
     event Voted(address indexed voter, bool support);
+
+    /// @notice Vote on the proposal
+    /// @param support true = yes, false = no
+    function vote(bool support) external {
+        require(!hasVoted[msg.sender], "Already voted");
+
+        hasVoted[msg.sender] = true;
+
+        if (support) {
+            yesVotes += 1;
+        } else {
+            noVotes += 1;
+        }
+
+        emit Voted(msg.sender, support);
+    }
 }
