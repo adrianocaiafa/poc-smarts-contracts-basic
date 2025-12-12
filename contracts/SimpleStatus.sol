c5f567550aa346996ac0c2a1d32ede6fa1f31618
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 14:27:52 -0300

    feat: SimpleStatus add generic metrics

--- contracts/SimpleStatus.sol
+++ contracts/SimpleStatus.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.30;
 /// @title SimpleStatus – on-chain status message per user
 /// @author acaiafa.base.eth
 contract SimpleStatus {
+    // -------------------------------------------------------------------------
+    // GENERIC INTERACTION METRICS
+    // -------------------------------------------------------------------------
+
+    uint256 public totalUniqueUsers;
+    mapping(address => bool) public hasInteracted;
+    mapping(address => uint256) public interactionsCount;    
 }
