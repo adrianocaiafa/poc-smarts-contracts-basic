a3da4d71b9b8437d6a1a2dc5eebfbb3f90ca5c4e
adrianocaiafa <acaiafa@hotmail.com>
2025-12-12 11:31:50 -0300

    feat: SimplePing add ping

--- contracts/SimplePing.sol
+++ contracts/SimplePing.sol
@@ -7,5 +7,15 @@ contract SimplePing {
 mapping(address => uint256) public pingCount;
     mapping(address => uint256) public lastPingAt;
 
-    uint256 public totalPings;    
+    uint256 public totalPings;
+
+    event Ping(address indexed user, uint256 count, uint256 timestamp);
+
+    function ping() external {
+        pingCount[msg.sender] += 1;
+        lastPingAt[msg.sender] = block.timestamp;
+        totalPings += 1;
+
+        emit Ping(msg.sender, pingCount[msg.sender], block.timestamp);
+    }
 }
